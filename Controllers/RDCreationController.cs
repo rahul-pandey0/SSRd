a0c3992cf6705580ec5b38b85906cc0d5fdb3e4a
Smitha  Shankara <smitha.s@techverves.in>
2026-05-09 11:16:49 +0530

    Rd creation

---
+++ Controllers/RDCreationController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SSRd.DTOs;
+using SSRd.Services;
+
+namespace SSRd.Controllers
+{
+    public class RDCreationController : Controller
+    {
+        private readonly IMembershipService _auth;
+        private readonly ILogger<RDCreationController> _logger;
+
+        public RDCreationController(IMembershipService auth, ILogger<RDCreationController> logger)
+        {
+            _auth = auth;
+            _logger = logger;
+        }
+
+        [HttpPost("RDcareation")]
+        public async Task<IActionResult> Register([FromBody] RDCreateRequest request)
+        {
+            try
+            {
+                var result = await _auth.RegisterAsync(request);
+                return Ok(ApiResponse<RDResponse>.Ok(result, result.Message));
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ApiResponse<object>.Fail(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, ApiResponse<object>.Fail("Something went wrong. Please try again."));
+            }
+        }
+
+    }
+}
---
+++ Models/TtRdDeposit.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SSRd.Models
+{
+    [Table("tt_rddeposits")]
+    public class TtRdDeposit
+    {
+        [Key]
+        public int RdDepositsId { get; set; }
+
+        [StringLength(16)]
+        public string? RdRefNumber { get; set; }
+
+        [StringLength(30)]
+        public string? MEMBERSHIP_NO { get; set; }
+
+        [StringLength(15)]
+        public string? Deposit_Type { get; set; }
+
+        [StringLength(45)]
+        public string? Product_Code { get; set; }
+
+        public double? Deposit_Amount { get; set; }
+
+        public int? Tenor { get; set; }
+
+        public DateTime? value_Date { get; set; }
+
+        public DateTime? Book_Date { get; set; }
+
+        public DateTime? Liquidation_Date { get; set; }
+
+        [StringLength(20)]
+        public string? Deposit_Account { get; set; }
+
+        [StringLength(1)]
+        public string? AuthStatus { get; set; }
+    }
+}
